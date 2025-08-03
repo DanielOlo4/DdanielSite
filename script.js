20bd08b7a6f187dba825de94e921c645885e9340
DanielOlo4 <balogundaniel130@gmail.com>
2025-08-03 19:18:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//  <!-- JavaScript to switch visibility -->
+    // Simulate login for demonstration
+    document.getElementById("login-form").addEventListener("submit", function (e) {
+      e.preventDefault();
+      document.getElementById("auth-section").style.display = "none";
+      document.getElementById("profile-section").style.display = "block";
+    });
+
+    document.getElementById("signup-form").addEventListener("submit", function (e) {
+      e.preventDefault();
+      document.getElementById("auth-section").style.display = "none";
+      document.getElementById("profile-section").style.display = "block";
+    });
+ 
